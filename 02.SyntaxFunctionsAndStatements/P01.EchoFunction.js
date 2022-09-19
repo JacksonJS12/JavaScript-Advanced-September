8978418ceee26e0aa5b1b819c4ce3a89483ef8a9
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-09-19 22:34:04 +0300

    commit

--- 02.SyntaxFunctionsAndStatements/P01.EchoFunction.js
+++ 02.SyntaxFunctionsAndStatements/P01.EchoFunction.js
@@ -1,8 +1,10 @@
+'use strict';
+
 function echo(inputAsString) {
     let stringLength = inputAsString.length;
 
     console.log(stringLength);
     console.log(inputAsString);
 }
-
-echo();
+let input = readline.question(string, callback());
+echo(input);
