fb338aa953b222d62c4304f2130de2aebe093856
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-09-20 21:15:12 +0300

    better quality

--- 02.SyntaxFunctionsAndStatements/P03.LargestNumber.js
+++ 02.SyntaxFunctionsAndStatements/P03.LargestNumber.js
@@ -1,22 +1,17 @@
 function largestNumber(numOne, numTwo, numThree){
     let largestNum = 0;
-    if(numOne > numTwo){
-        if(numOne > numThree){
-            largestNum = numOne;
-        }
-        else{
-            largestNum = numThree
-        }
+    
+    if(numOne > numTwo && numOne > numThree){
+        largestNum = numOne
     }
-    else{
-        if(numTwo > numThree){
-            largestNum = numOne;
-        }
-        else{
-            largestNum = numThree
-        }
+    else if(numTwo > numOne && numTwo > numThree){
+        largestNum = numTwo;
     }
-    console.log('The largest number is ' + largestNum + '.');
+    else if(numThree > numOne && numThree > numTwo){
+        largestNum = numThree;
+    }
+
+    console.log(`The largest number is ${largestNum}.`);
 }
 
 largestNumber(-3, -5, -22.5);
