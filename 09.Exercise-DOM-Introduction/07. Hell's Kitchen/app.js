31331c2639cb48e0ae8dda8c7c240481a773cdf3
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2022-10-09 23:16:25 +0300

    commit

--- 09.Exercise-DOM-Introduction/07. Hell's Kitchen/app.js
+++ 09.Exercise-DOM-Introduction/07. Hell's Kitchen/app.js
@@ -2,7 +2,29 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      let input = JSON.parse(document.getElementById("inputs").children[1].value);
+
+      for(let data of input) {
+         let [name, workerList] = data.split(" - ");
+         if((!result.find(e=> e.name === name)) {
+            result.push({ 
+               name,
+               avgSalary : 0,
+               bestSalary : 0,
+               sumSalary : 0,
+               workerList : []
+            });
+            let current = result.find(e=>e.name === name);
+            for(let currentWorker of workerList){
+               updateRestaurant(currentRestaurant, currentWorker)
+            }
+         }
+
+      }
+      let dataArr = input.split(" - ")
+      
+   }
+   function updateRestaurant(obj, worker) {
       
    }
 }
