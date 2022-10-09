ed882ee7735654e4108a0c1bbba91ec8a4a7a213
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2022-10-09 21:47:55 +0300

    commit

--- 09.Exercise-DOM-Introduction/06. Format the Text/solution.js
+++ 09.Exercise-DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  let input = document.getElementById("input").vale;
+  let output = document.getElementById("output");
+  output.innerHTML = "";
+  let arrayText = input.split(".");
+
+  for(let i = 0;i < arrayText.length; i+=3) {
+    let res = [];
+    for(let x = 0; x < 3; x++) {
+        if(arrayText[i+x]) {
+          res.push(arrayText[i+x]);
+        }
+    }
+    let resText = res.join(". ") + ".".trim();
+    output.innerHTML += `<p>${resText}</p>`
+  }
 }
