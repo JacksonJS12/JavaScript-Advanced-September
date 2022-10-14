7385facb3924c482a6cd00402bfc07d9b9cbe3d6
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2022-10-14 22:17:01 +0300

    fix

---
+++ 13.Exercise-Advanced-Functions/P03.Fibonacci/P03.Fibonacci.js
@@ -0,0 +1,11 @@
+function getFibonator(){
+    let firstNum = 0;
+    let secNum = 1;
+
+    return function(){
+        let sum = firstNum + secNum;
+        firstNum = secNum;
+        secNum = sum;
+        return firstNum
+    }
+}
