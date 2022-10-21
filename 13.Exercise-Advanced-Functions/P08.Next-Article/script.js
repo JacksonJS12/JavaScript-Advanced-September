78909fa2b13aacd5ef46e94cd2621f6a0020e9b5
Deniz <d.ivan0v.6100@gmail.com>
2022-10-21 16:06:41 +0300

    more exercises added

--- 13.Exercise-Advanced-Functions/P08.Next-Article/script.js
+++ 13.Exercise-Advanced-Functions/P08.Next-Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let articlesArray = articles;
+
+    return () => {
+        let contentElement = document.getElementById("content");
+        let newArtcl = document.createElement("article");
+        if (articlesArray.length > 0) {
+            newArtcl.textContent = articlesArray.shift();
+            contentElement.appendChild(newArtcl);
+        }
+    }
+
 }
